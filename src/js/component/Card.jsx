730f4de33bd98d9e0fd236419adacc76d5339fb1
Alberto Valtierra Jr <betomasia12@gmail.com>
2022-12-17 01:54:05 +0000

    fixed everything, just need to fix card code

---
+++ src/js/component/Card.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+export const Card = props => {
+    return (
+    <div className="card" style="width: 18rem;">
+		<img src="..." class="card-img-top" alt="..."/>
+		<div className="card-body">
+			<h5 className="card-title">Card title</h5>
+			<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			<a href="#" class="btn btn-primary">Go somewhere</a>
+		</div>
+	</div>
+)};
+
+
+const container = () => {
+    <div>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="..."/>
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="..."/>
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="..."/>
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="..."/>
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    
+    <div class="footer">
+        <p>Copyright Your Website 2022</p>
+    </div>
+    </div>
+};
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import Card from "./Card";
+import {Card} from "./Card.jsx";
+import { Jumbotron } from "./Jumbotron.jsx";
+import {Navbar} from "./Navbar.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -7,44 +9,19 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="container">
-		 	<nav className="navbar navbar-expand-lg navbar-light bg-light">
-		 		<div className="container-fluid">
-		 		<a className="navbar-brand" href="#">Navbar</a>
-		 		<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-		 			<span className="navbar-toggler-icon"></span>
-		 		</button>
-		 		<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-		 			<div className="navbar-nav">
-		 			<a className="nav-link active" aria-current="page" href="#">Home</a>
-		 			<a className="nav-link" href="#">Features</a>
-		 			<a className="nav-link" href="#">Pricing</a>
-		 			<a className="nav-link disabled">Disabled</a>
-		 			</div>
-		 		</div>
-				</div>
-		 	</nav> 
+        <div>
+        <Navbar />
+		    <div className="container">
+                <Jumbotron />
+                <Card />
+                <Card />
+                <Card />
 
-			{/* <div class="jumbotron">
-				<h1 class="display-4">Hello, world!</h1>
-				<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-				<hr class="my-4"></hr>
-				<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-				<p class="lead">
-					<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-				</p>
-			</div> */}
-
-			<div>
-				<Card/>
-				<Card/>
-				<Card/>
-			</div>
-			
-			<footer className="footer">
-				<p>Copyright Your Website 2022</p>
-			</footer> 
-		</div>
+		    </div>
+        <footer className="footer">
+            <p>Copyright Your Website 2022</p>
+        </footer>
+        </div>
 	)
 }
 export default Home;
