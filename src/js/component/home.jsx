a14dbc111b0e363e38985393979448f4e3a1398d
Alberto Valtierra Jr <betomasia12@gmail.com>
2022-12-26 03:13:01 +0000

    removed import of riggs photos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,7 +4,7 @@ import { Jumbotron } from "./Jumbotron.jsx";
 import { Navbar } from "./Navbar.jsx";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
