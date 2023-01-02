9627b960ac548054a4a8216b799ca3428eb83da8
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-01-02 00:41:25 +0000

    added a photo in the first slide of the jumbotron

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -7,17 +7,16 @@ export const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container">
       <a className="navbar-brand" href="#">
-        Start React
+        Alberto Valtierra Jr
       </a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav ms-auto">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-          <a className="nav-link" href="#">Features</a>
-          <a className="nav-link" href="#">Pricing</a>
-          <a className="nav-link" href="#">Contact</a>
+          <a className="nav-link active" aria-current="page" href="#">Aout Me</a>
+          <a className="nav-link" href="#">Work</a>
+          <a className="nav-link" href="#">Contact Me</a>
         </div>
       </div>
     </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -12,13 +12,13 @@ const Home = () => {
       <Navbar />
       <div>
         <div>
-          <Carousel imageUrl="./Beto.jpg"
+          <Carousel imageUrl="https://images.unsplash.com/photo-1585076641399-5c06d1b3365f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aHRtbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
           imageUrl2="https://images.unsplash.com/photo-1499209974431-9dddcece7f88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aGFyZCUyMHdvcmt8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
           imageUrl3="https://images.unsplash.com/photo-1607523751915-5291fab91551?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGRpc2NpcGxpbmV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"/>
         </div>
       </div>
       <footer className="footer bg-dark p-3">
-        <p className="text-center text-white">Betos Money 2022</p>
+        <p className="text-center text-white">Alberto Valtierra Jr</p>
       </footer>
     </div>
   );
