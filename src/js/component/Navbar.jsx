5ebe8cfc91c55446e412b5d6def1ebcba1b800c4
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-01-06 18:56:08 +0000

    changing the style of the navbar, taking away the dropdown and instead of having my name, putting a place holder for a logo

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -7,18 +7,18 @@ export const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light">
     <div className="container">
       <a className="navbar-brand" href="#">
-        Alberto Valtierra Jr
+        Logo Placement
       </a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      {/* <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav ms-auto">
           <a className="nav-link active" aria-current="page" href="#">Aout Me</a>
           <a className="nav-link" href="#">Work</a>
           <a className="nav-link" href="#">Contact Me</a>
         </div>
-      </div>
+      </div> */}
     </div>
   </nav>
     )
