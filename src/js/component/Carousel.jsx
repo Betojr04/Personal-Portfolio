88a5af5c3f09f0d43da743a0adeab59dfed286b6
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-01-06 19:18:53 +0000

    added three props and and changed descriptions, image urls and titles

--- src/js/component/Carousel.jsx
+++ src/js/component/Carousel.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 export const Carousel = (props) => {
-  const cardProps = [{projectTitle: "Excuse Generator", projectImage: "imageUrl1" , projectDescripion: "generates random xcuses"}]
+  const cardProps = [
+    {
+      projectTitle: "Excuse Generator",
+      projectImage:
+        "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      projectDescripion: "generates random xcuses",
+    },
+    {
+      projectTitle: "Caculator",
+      projectImage:
+        "https://media.istockphoto.com/id/1359420140/photo/unrecognisable-man-going-over-his-finances.jpg?s=612x612&w=0&k=20&c=EHal7J50ElwrYwpvHU5lAnyWambCkXctUupo-SSxzmQ=",
+      projectDescripion: "calculates numbers",
+    },
+    {
+      projectTitle: "To do list",
+      projectImage:
+        "https://images.unsplash.com/photo-1598791318878-10e76d178023?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dG8lMjBkbyUyMGxpc3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+      projectDescripion: "todos",
+    },
+  ];
 
   return (
     <div
@@ -35,7 +54,10 @@ export const Carousel = (props) => {
         <div className="carousel-item active">
           <img src={props.imageUrl} className="d-block w-100" alt="..." />
           <div className="carousel-caption d-flex align-items-center justify-content-between text-end h-100">
-            <img className="img-fluid col-4" src="https://images.unsplash.com/photo-1513682121497-80211f36a7d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" />
+            <img
+              className="img-fluid col-4"
+              src="https://images.unsplash.com/photo-1513682121497-80211f36a7d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            />
             <div className="">
               <h3>About Me</h3>
               <p>Full-Stack Software Developer</p>
